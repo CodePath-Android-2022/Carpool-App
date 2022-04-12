cd01358238d0d43027e968ee93ce3dc80006e665
Benaldofire <36219766+Benaldofire@users.noreply.github.com>
2022-04-12 00:38:35 -0500

    Add files via upload

---
+++ Carpool/app/src/main/java/com/example/carpool/User.kt
@@ -0,0 +1,17 @@
+package com.example.carpool
+
+import com.parse.ParseClassName
+import com.parse.ParseObject
+
+@ParseClassName("User")
+class User : ParseObject() {
+    //create the getters and setter for the following
+
+    //description
+    //sourceLocation
+    //destinationLocation
+    //user
+    //price
+    //departureDate
+
+}
