d81984e8805424cb7fad34cfe9de22da41a64cba
Benaldofire <Benedict.Odai@coyotes.usd.edu>
2022-04-12 16:37:41 -0500

    Finished Setting up the parse backend, and completed the base code for the bottom navigation and it's respective fragments.

--- Carpool/app/src/main/java/com/example/carpool/LoginActivity.kt
+++ Carpool/app/src/main/java/com/example/carpool/LoginActivity.kt
@@ -1,11 +1,53 @@
 package com.example.carpool
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.parse.ParseObject
+import com.parse.ParseUser
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //check if user's already logged in
+        //if true, take them to MainActivity
+        if(ParseUser.getCurrentUser() != null) {
+            goToMainActivity()
+        }
+
+        findViewById<Button>(R.id.login_button).setOnClickListener {
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+
+            loginUser(username, password)
+        }
+    }
+
+    //Test login:
+    //username: john_doe
+    //password: john123
+
+    fun loginUser(username: String, password: String) {
+        ParseUser.logInInBackground(username, password, ({ user, e->
+            if (user != null) {
+                Toast.makeText(this, "Successfully Logged In", Toast.LENGTH_LONG).show()
+                goToMainActivity()
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error loggin in", Toast.LENGTH_LONG).show()
+            }
+        }))
+    }
+
+    private fun goToMainActivity() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- Carpool/app/src/main/java/com/example/carpool/MainActivity.kt
+++ Carpool/app/src/main/java/com/example/carpool/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.carpool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.carpool.fragments.ComposeFragment
+import com.example.carpool.fragments.FeedFragment
+import com.example.carpool.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+             var fragmentToDisplay: Fragment? = null
+            when (item.itemId) {
+                R.id.action_home -> {
+                    fragmentToDisplay = FeedFragment()
+                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                }
+                R.id.action_compose -> {
+                    fragmentToDisplay = ComposeFragment()
+                    Toast.makeText(this, "Compose Ride", Toast.LENGTH_SHORT).show()
+                }
+                R.id.action_profile -> {
+                    fragmentToDisplay = ProfileFragment()
+                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            if (fragmentToDisplay != null) {
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToDisplay).commit()
+            }
+            true
+        }
+
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.action_home
     }
 }
