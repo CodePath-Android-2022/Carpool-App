4082acbd4edd48c3487f15d011169ca6b7c6c1b0
Benaldofire <Benedict.Odai@coyotes.usd.edu>
2022-04-14 14:28:22 -0500

    stephen's branch created

--- Carpool/app/src/main/java/com/example/carpool/CarpoolPost.kt
+++ Carpool/app/src/main/java/com/example/carpool/CarpoolPost.kt
@@ -5,6 +5,8 @@ import com.parse.ParseObject
 
 @ParseClassName("CarpoolPost")
 class CarpoolPost : ParseObject () {
+
+    //branch to push to ***- stephen -**
     //create the getters and setter for the following
 
     //description
