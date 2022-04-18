92993b119bc266f778181461de8f77d13462fbf5
sehumah <sehumah@protonmail.com>
2022-04-17 22:14:15 -0500

    fill username view of compose page with current user's username

--- Carpool/app/src/main/java/com/example/carpool/fragments/ComposeFragment.kt
+++ Carpool/app/src/main/java/com/example/carpool/fragments/ComposeFragment.kt
@@ -5,17 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.carpool.R
+import com.parse.ParseUser
 
 
 /**
  * Add functionality so users can create a new Carpool
  */
 class ComposeFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // fill username view with current user's username
+        container?.findViewById<TextView>(R.id.tv_username)?.text = ParseUser.getCurrentUser().username
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_compose, container, false)
     }
