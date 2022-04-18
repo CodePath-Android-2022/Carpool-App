a8c4b4b947a6942190b5bb44904b5a58c679c87e
sehumah <sehumah@protonmail.com>
2022-04-17 23:06:48 -0500

    move code for setting username into onviewcreated function, grab reference to all edit text fields and add another parameter to function to submit carpool post to server

--- Carpool/app/src/main/java/com/example/carpool/fragments/ComposeFragment.kt
+++ Carpool/app/src/main/java/com/example/carpool/fragments/ComposeFragment.kt
@@ -20,13 +20,8 @@ import java.util.*
  * Add functionality so users can create a new Carpool
  */
 class ComposeFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        // fill username view with current user's username
-        container?.findViewById<TextView>(R.id.tv_username)?.text = ParseUser.getCurrentUser().username
-
-        Log.i("ComposeFragment", "current logged in user: ${ParseUser.getCurrentUser().username}")
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_compose, container, false)
     }
@@ -36,47 +31,39 @@ class ComposeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        // get reference to the start location edit text view
-        view.findViewById<EditText>(R.id.et_start_location).setOnClickListener {
-            // grab the start location the user has inputted
-        }
-
-        // get reference to the destination location edit text view
-        view.findViewById<EditText>(R.id.et_destination_location).setOnClickListener {
-            // grab the destination location the user has inputted
-        }
-
-        // get reference to the departure date edit text view
-        view.findViewById<EditText>(R.id.et_departure_date).setOnClickListener {
-            // grab the departure date the user has inputted
-        }
-
-        // get reference to the car capacity edit text view
-        view.findViewById<EditText>(R.id.et_car_capacity).setOnClickListener {
-            // grab the car capacity the user has inputted
-        }
-
-        // get reference to the trip description edit text view
-        view.findViewById<EditText>(R.id.et_trip_description).setOnClickListener {
-            // grab the trip description the user has inputted
-        }
-
-        // get reference to the price edit text view
-        view.findViewById<EditText>(R.id.et_trip_price).setOnClickListener {
-            // grab the price the user has inputted
-        }
+        // fill username view with current user's username
+        view.findViewById<TextView>(R.id.tv_username).text = ParseUser.getCurrentUser().username
 
         // get reference to the create carpool button view
         view.findViewById<Button>(R.id.btn_create_carpool).setOnClickListener {
             // todo: submit info to the home page after the user has filled all required information
+
+            // grab the start location the user has inputted
+            val startLocation = view.findViewById<EditText>(R.id.et_start_location).text.toString()
+
+            // grab the destination location the user has inputted
+            val destination = view.findViewById<EditText>(R.id.et_destination_location).text.toString()
+
+            // grab the departure date the user has inputted
+            val departureDate = view.findViewById<EditText>(R.id.et_departure_date).text.toString()
+
+            // grab the car capacity the user has inputted
+            val carCapacity = view.findViewById<EditText>(R.id.et_car_capacity).text.toString()
+
+            // grab the trip description the user has inputted
+            val tripDescription = view.findViewById<EditText>(R.id.et_trip_description).text.toString()
+
+            // grab the price the user has inputted
+            val price = view.findViewById<EditText>(R.id.et_trip_price).text.toString()
+
+
             Toast.makeText(context, "Create Carpool Button Clicked!", Toast.LENGTH_SHORT).show()
         }
     }
 
 
     // function to submit created carpool post to server
-    private fun submitCarpoolPost(startLocation: String, destination: String, departureDate: Date, carCapacity: Int, tripDescription: String, price: Int) {
+    private fun submitCarpoolPost(startLocation: String, destination: String, departureDate: Date, carCapacity: Int, tripDescription: String, price: Int, user: ParseUser) {
 
         // create a Carpool post object to send to server
         val carpoolPost = CarpoolPost()
