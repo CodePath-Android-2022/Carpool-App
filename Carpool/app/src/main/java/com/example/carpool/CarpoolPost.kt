7eb6632f5a56c0c5054bc69192ff5658f3c69a03
sehumah <sehumah@protonmail.com>
2022-04-17 23:47:29 -0500

    refactor keys and values and rearrange getter and setter functions

--- Carpool/app/src/main/java/com/example/carpool/CarpoolPost.kt
+++ Carpool/app/src/main/java/com/example/carpool/CarpoolPost.kt
@@ -8,70 +8,71 @@ import com.parse.ParseUser
 class CarpoolPost : ParseObject () {
 
     companion object {
-        private const val KEY_START_LOCATION: String = "startLocation"
-        private const val KEY_DESTINATION: String = "destination"
-        private const val KEY_DESCRIPTION: String = "description"
+        private const val KEY_USER: String = "user"
+        private const val KEY_SOURCE_LOCATION: String = "sourceLocation"
+        private const val KEY_DESTINATION_LOCATION: String = "destinationLocation"
         private const val KEY_DEPARTURE_DATE: String = "departureDate"
         private const val KEY_CAR_CAPACITY: String = "carCapacity"
+        private const val KEY_DESCRIPTION: String = "description"
         private const val KEY_PRICE: String = "price"
-        private const val KEY_USER: String = "user"
     }
 
     /**
      *  create the getters and setter for the following:
-     *      - startLocation
-     *      - destination
-     *      - departureDate
-     *      - description
-     *      - seats
-     *      - price
-     *      - user
+     *  -   user: Pointer<User>
+     *  -   sourceLocation: String
+     *  -   destinationLocation: String
+     *  -   departureDate: Date
+     *  -   carCapacity: Number
+     *  -   description: String
+     *  -   price: Number
      */
+
 
     /*  Setters  */
 
-    fun setStartLocation(startLocation: String) {
-        put(KEY_START_LOCATION, startLocation)
+    fun setUser(user: User){
+        put(KEY_USER, user)
     }
 
-    fun setDestination(destination: String) {
-        put(KEY_DESTINATION, destination)
+    fun setSourceLocation(sourceLocation: String) {
+        put(KEY_SOURCE_LOCATION, sourceLocation)
     }
 
-    fun setDescription(description: String) {
-        put(KEY_DESCRIPTION, description)
+    fun setDestinationLocation(destinationLocation: String) {
+        put(KEY_DESTINATION_LOCATION, destinationLocation)
     }
 
     fun setDepartureDate(departureDate: String) {
         put(KEY_DEPARTURE_DATE, departureDate)
     }
 
-    fun setCarCapacity(seats: Int) {
-        put(KEY_CAR_CAPACITY, seats)
+    fun setCarCapacity(carCapacity: Int) {
+        put(KEY_CAR_CAPACITY, carCapacity)
     }
 
-    fun setPrice(price: String) {
+    fun setDescription(description: String) {
+        put(KEY_DESCRIPTION, description)
+    }
+
+    fun setPrice(price: Int) {
         put(KEY_PRICE, price)
-    }
-
-    fun setUser(user: User){
-        put(KEY_USER, user)
     }
 
 
 
     /*  Getters  */
 
-    fun getStartLocation() : String? {
-        return getString(KEY_START_LOCATION)
+    fun getUser() : ParseUser? {
+        return getParseUser(KEY_USER)
     }
 
-    fun getDestination() : String? {
-        return getString(KEY_DESTINATION)
+    fun getSourceLocation() : String? {
+        return getString(KEY_SOURCE_LOCATION)
     }
 
-    fun getDescription() : String? {
-        return getString(KEY_DESCRIPTION)
+    fun getDestinationLocation() : String? {
+        return getString(KEY_DESTINATION_LOCATION)
     }
 
     fun getDepartureDate() : String? {
@@ -82,12 +83,12 @@ class CarpoolPost : ParseObject () {
         return getInt(KEY_CAR_CAPACITY)
     }
 
-    fun getPrice() : String? {
-        return getString(KEY_PRICE)
+    fun getDescription() : String? {
+        return getString(KEY_DESCRIPTION)
     }
 
-    fun getUser() : ParseUser? {
-        return getParseUser(KEY_USER)
+    fun getPrice() : Int {
+        return getInt(KEY_PRICE)
     }
 
 }
