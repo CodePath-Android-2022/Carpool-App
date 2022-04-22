ff50d15a8727d6822fc7710e9ff6325e246cbb18
Benaldofire <Odaibenedict@gmail.com>
2022-04-22 03:24:34 -0500

    Merged stephen's code with new layout

--- Carpool/app/src/main/java/com/example/carpool/CarpoolPost.kt
+++ Carpool/app/src/main/java/com/example/carpool/CarpoolPost.kt
@@ -8,86 +8,97 @@ import com.parse.ParseUser
 class CarpoolPost : ParseObject () {
 
     companion object {
-        private const val KEY_START_LOCATION: String = "startLocation"
-        private const val KEY_DESTINATION: String = "destination"
-        private const val KEY_DESCRIPTION: String = "description"
-        private const val KEY_DEPARTURE_DATE: String = "departureDate"
-        private const val KEY_NUMBER_OF_SEATS: String = "seats"
-        private const val KEY_PRICE: String = "price"
         private const val KEY_USER: String = "user"
+        private const val KEY_SOURCE_LOCATION: String = "sourceLocation"
+        private const val KEY_DESTINATION_LOCATION: String = "destinationLocation"
+        private const val KEY_DEPARTURE_DATE: String = "departureDate"
+        private const val KEY_DEPARTURE_TIME: String = "departureTime"
+        private const val KEY_CAR_CAPACITY: String = "carCapacity"
+        private const val KEY_DESCRIPTION: String = "description"
+        private const val KEY_PRICE: String = "price"
     }
 
     /**
      *  create the getters and setter for the following:
-     *      - startLocation
-     *      - destination
-     *      - departureDate
-     *      - description
-     *      - seats
-     *      - price
-     *      - user
+     *  -   user: Pointer<User>
+     *  -   sourceLocation: String
+     *  -   destinationLocation: String
+     *  -   departureDate: Date todo: change to String later, if we don't get it to work
+     *  -   departureTime: String
+     *  -   carCapacity: Number
+     *  -   description: String
+     *  -   price: Number
      */
+
 
     /*  Setters  */
 
-    fun setStartLocation(startLocation: String) {
-        put(KEY_START_LOCATION, startLocation)
+    fun setUser(user: ParseUser){
+        put(KEY_USER, user)
     }
 
-    fun setDestination(destination: String) {
-        put(KEY_DESTINATION, destination)
+    fun setSourceLocation(sourceLocation: String) {
+        put(KEY_SOURCE_LOCATION, sourceLocation)
     }
 
-    fun setDescription(description: String) {
-        put(KEY_DESCRIPTION, description)
+    fun setDestinationLocation(destinationLocation: String) {
+        put(KEY_DESTINATION_LOCATION, destinationLocation)
     }
 
     fun setDepartureDate(departureDate: String) {
         put(KEY_DEPARTURE_DATE, departureDate)
     }
 
-    fun setNumberOfSeats(seats: Int) {
-        put(KEY_NUMBER_OF_SEATS, seats)
+    fun setDepartureTime(departureTime: String) {
+        put(KEY_DEPARTURE_TIME, departureTime)
     }
 
-    fun setPrice(price: String) {
+    fun setCarCapacity(carCapacity: Int) {
+        put(KEY_CAR_CAPACITY, carCapacity)
+    }
+
+    fun setDescription(description: String) {
+        put(KEY_DESCRIPTION, description)
+    }
+
+    fun setPrice(price: Float) {
         put(KEY_PRICE, price)
-    }
-
-    fun setUser(user: User){
-        put(KEY_USER, user)
     }
 
 
 
     /*  Getters  */
 
-    fun getStartLocation() : String? {
-        return getString(KEY_START_LOCATION)
+    fun getUser() : ParseUser? {
+        return getParseUser(KEY_USER)
     }
 
-    fun getDestination() : String? {
-        return getString(KEY_DESTINATION)
+    fun getSourceLocation() : String? {
+        return getString(KEY_SOURCE_LOCATION)
     }
 
-    fun getDescription() : String? {
-        return getString(KEY_DESCRIPTION)
+    fun getDestinationLocation() : String? {
+        return getString(KEY_DESTINATION_LOCATION)
     }
 
     fun getDepartureDate() : String? {
         return getString(KEY_DEPARTURE_DATE)
     }
 
-    fun getNumberOfSeats() : Int {
-        return getInt(KEY_NUMBER_OF_SEATS)
+    fun getDepartureTime() : String? {
+        return getString(KEY_DEPARTURE_TIME)
     }
 
-    fun getPrice() : String? {
-        return getString(KEY_PRICE)
+    fun getCarCapacity() : Int {
+        return getInt(KEY_CAR_CAPACITY)
     }
 
-    fun getUser() : ParseUser? {
-        return getParseUser(KEY_USER)
+    fun getDescription() : String? {
+        return getString(KEY_DESCRIPTION)
+    }
+
+    fun getPrice() : Int {
+        return getInt(KEY_PRICE)
     }
 
 }
--- Carpool/app/src/main/java/com/example/carpool/fragments/ComposeFragment.kt
+++ Carpool/app/src/main/java/com/example/carpool/fragments/ComposeFragment.kt
@@ -1,18 +1,220 @@
 package com.example.carpool.fragments
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+import com.example.carpool.CarpoolPost
 import com.example.carpool.R
+import com.parse.ParseUser
+import java.util.*
 
-class ComposeFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+
+/**
+ * Add functionality so users can create a new Carpool post and submit the post to the server to be displayed on the home page
+ */
+
+private const val TAG = "ComposeFragment"
+
+class ComposeFragment : Fragment(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    // declare variables for views in xml layout file
+    private lateinit var tvUsername: TextView
+    private lateinit var etSourceLocation: EditText
+    private lateinit var etDestinationLocation: EditText
+    private lateinit var btnDepartureDate: Button
+    private lateinit var tvDepartureDate: TextView
+    private lateinit var btnDepartureTime: Button
+    private lateinit var tvDepartureTime: TextView
+    private lateinit var etCarCapacity: EditText
+    private lateinit var etDescription: EditText
+    private lateinit var etPrice: EditText
+    private lateinit var btnCreateCarpool: Button
+
+    // create variables for the date and time info
+    var day = 0
+    var month = 0
+    var year = 0
+    var hour = 0
+    var minute = 0
+
+    var savedDay = 0
+    var savedMonth = 0
+    var savedYear = 0
+    var savedHour = 0
+    var savedMinute = 0
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_compose, container, false)
+    }
+
+
+    // set onclick listeners and setup logic
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // get reference to all text views and edit text fields
+        tvUsername = view.findViewById(R.id.tv_username)
+        etSourceLocation = view.findViewById(R.id.et_start_location)
+        etDestinationLocation = view.findViewById(R.id.et_destination_location)
+        btnDepartureDate = view.findViewById(R.id.btn_departure_date)
+        tvDepartureDate = view.findViewById(R.id.tv_departure_date)
+        btnDepartureTime = view.findViewById(R.id.btn_departure_time)
+        tvDepartureTime = view.findViewById(R.id.tv_departure_time)
+        etCarCapacity = view.findViewById(R.id.et_car_capacity)
+        etDescription = view.findViewById(R.id.et_trip_description)
+        etPrice = view.findViewById(R.id.et_trip_price)
+        btnCreateCarpool = view.findViewById(R.id.btn_create_carpool)
+
+
+        // fill username view with current user's username
+        tvUsername.text = ParseUser.getCurrentUser().username
+
+        // set a click listener on the departure date button to display the date picker
+        btnDepartureDate.setOnClickListener {
+            pickDate()
+        }
+
+        // set a click listener on the departure time button to display the time picker
+        btnDepartureTime.setOnClickListener {
+            pickTime()
+        }
+
+
+        // get reference to the create carpool button view
+        btnCreateCarpool.setOnClickListener {
+
+            // grab all the info the user has inputted
+            val sourceLocation = etSourceLocation.text.toString()
+            val destinationLocation = etDestinationLocation.text.toString()
+            val departureDate = tvDepartureDate.text.toString()
+            val departureTime = tvDepartureTime.text.toString()
+            val carCapacity = etCarCapacity.text.toString()
+            val description = etDescription.text.toString()
+            val price = etPrice.text.toString()
+
+            // check that all fields are filled & submit info to the home page after the user has filled all required information
+            if (sourceLocation.isEmpty() || destinationLocation.isEmpty() || departureDate.isEmpty() ||  departureTime.isEmpty() ||  carCapacity.isEmpty() ||  description.isEmpty() ||  price.isEmpty()) {  // some or all fields are empty
+                Toast.makeText(context, "Please fill all fields!", Toast.LENGTH_SHORT).show()
+            }
+            else if (sourceLocation.isNotEmpty() && destinationLocation.isNotEmpty() && departureDate.isNotEmpty() &&  departureTime.isNotEmpty() &&  carCapacity.isNotEmpty() &&  description.isNotEmpty() &&  price.isNotEmpty()) {  // all fields are filled
+
+                // log the user's typed info to logcat before submitting the post to server and clearing all fields
+                Log.i(TAG,  "$sourceLocation, $destinationLocation, $departureDate, $departureTime, $carCapacity, $description, $price")
+
+                // send the created post to the server
+                submitCarpoolPostToServer(ParseUser.getCurrentUser(), sourceLocation, destinationLocation, departureDate, departureTime, carCapacity.toInt(), description, price.toFloat())
+
+                // empty all edit text fields after post is saved
+                etSourceLocation.setText("")
+                etDestinationLocation.setText("")
+                tvDepartureDate.setText("")
+                tvDepartureTime.setText("")
+                etCarCapacity.setText("")
+                etDescription.setText("")
+                etPrice.setText("")
+            }
+        }
+    }
+
+
+    // function to submit created carpool post to server
+    private fun submitCarpoolPostToServer(user: ParseUser, sourceLocation: String, destinationLocation: String, departureDate: String, departureTime: String, carCapacity: Int, description: String, price: Float) {
+
+        /**
+         * WHAT'S IN THE SERVER
+         * --------------------
+         * user: Pointer<User>
+         * sourceLocation: String
+         * destinationLocation: String
+         * departureDate: Date
+         * departureTime: String
+         * carCapacity: Number
+         * description: String
+         * price: Number
+         */
+
+        // create a Carpool post object to be sent to the server
+        val carpoolPost = CarpoolPost()
+
+        // set all fields to save
+        carpoolPost.setUser(user)
+        carpoolPost.setSourceLocation(sourceLocation)
+        carpoolPost.setDestinationLocation(destinationLocation)
+        carpoolPost.setDepartureDate(departureDate)
+        carpoolPost.setDepartureTime(departureTime)
+        carpoolPost.setCarCapacity(carCapacity)
+        carpoolPost.setDescription(description)
+        carpoolPost.setPrice(price)
+
+        // save post to server
+        carpoolPost.saveInBackground { exception ->
+            if (exception == null) {  // everything is good
+                Toast.makeText(context, "Successfully saved post in server!", Toast.LENGTH_SHORT).show()
+                Log.i(TAG, "Successfully saved post in server!")
+            }
+            else {  // then something's gone wrong
+                Toast.makeText(context, "Something went wrong! Couldn't save post.", Toast.LENGTH_SHORT).show()
+                Log.e(TAG, "Something went wrong! Couldn't save post. Error message: ${exception.message}")  // todo: fix the ff error: "Something went wrong! Couldn't save post. Error message: schema mismatch for CarpoolPost.departureDate; expected Date but got String"
+            }
+        }
+    }
+
+
+    private fun getDateTimeCalendar() {
+        val calendar = Calendar.getInstance()
+        day = calendar.get(Calendar.DAY_OF_MONTH)
+        month = calendar.get(Calendar.MONTH)
+        year = calendar.get(Calendar.YEAR)
+        hour = calendar.get(Calendar.HOUR)
+        minute = calendar.get(Calendar.MINUTE)
+    }
+
+    private fun pickDate() {
+        getDateTimeCalendar()
+        DatePickerDialog(requireContext(), this, year, month, day).show()
+    }
+
+    private fun pickTime() {
+        getDateTimeCalendar()
+        TimePickerDialog(requireContext(), this, hour, minute, true).show()
+    }
+
+    override fun onDateSet(datePicker: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        savedDay = dayOfMonth
+        savedMonth = month
+        savedYear = year
+
+        getDateTimeCalendar()
+
+        val monthNames = arrayOf("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+        tvDepartureDate.text = "${monthNames.get(savedMonth)} $savedDay $savedYear"
+    }
+
+    override fun onTimeSet(timePicker: TimePicker?, hourOfDay: Int, minute: Int) {
+        savedHour = hourOfDay
+        savedMinute = minute
+
+        getDateTimeCalendar()
+
+        if (savedHour in 0..9 && savedMinute !in 0..9) {
+            tvDepartureTime.text = "0"+savedHour+":"+savedMinute
+        }
+        else if (savedMinute in 0..9 && savedHour !in 0..9) {
+            tvDepartureTime.text = ""+savedHour+":0"+savedMinute
+        }
+        else if (savedHour in 0..9 && savedMinute in 0..9) {
+            tvDepartureTime.text = "0"+savedHour+":0"+savedMinute
+        }
+        else {
+            tvDepartureTime.text = "$savedHour:$savedMinute"
+        }
     }
 }
