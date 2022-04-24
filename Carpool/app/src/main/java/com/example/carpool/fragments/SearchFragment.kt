4a3008060475f8a3b5387129189394a205ba9cdd
Benaldofire <Benedict.Odai@coyotes.usd.edu>
2022-04-24 13:26:52 -0500

    Working on search fragment's layout

--- Carpool/app/src/main/java/com/example/carpool/fragments/SearchFragment.kt
+++ Carpool/app/src/main/java/com/example/carpool/fragments/SearchFragment.kt
@@ -1,11 +1,14 @@
 package com.example.carpool.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.fragment.app.Fragment
 import com.example.carpool.R
+
 
 class SearchFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +17,9 @@ class SearchFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
