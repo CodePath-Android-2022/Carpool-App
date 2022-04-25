1a51d892c427c8ced77357215d3e063c0de9a245
Abhash404 <80597347+Abhash404@users.noreply.github.com>
2022-04-25 18:50:41 -0500

    Add files via upload

--- Carpool/app/src/main/java/com/example/carpool/ProfileActivity.kt
+++ Carpool/app/src/main/java/com/example/carpool/ProfileActivity.kt
@@ -22,10 +22,9 @@ import java.util.*
 
 
 class ProfileActivity : AppCompatActivity() {
-    
     private var databaseReference: DatabaseReference? = null
     private var profileNameTextView: TextView? = null
-    private var profileSurnameTextView: TextView? = null
+    private var profileEmailTextView: TextView? = null
     private var profilePhonenoTextView: TextView? = null
     private var profileAddressTextView: TextView? = null
     private var firebaseAuth: FirebaseAuth? = null
@@ -39,69 +38,63 @@ class ProfileActivity : AppCompatActivity() {
     private var firebaseStore: FirebaseStorage? = null
     private var storageReference: StorageReference? = null
     lateinit var imagePreview: ImageView
-    private var btn_choose_image: Button? = null
-    private var btn_upload_image: Button? = null
-    
+    private var btn_choose_image: ImageButton? = null
+    private var btn_upload_image: ImageButton? = null
     @SuppressLint("CutPasteId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         databaseReference = FirebaseDatabase.getInstance().reference
         editTextName = findViewById(R.id.et_username)
         profilePicImageView = findViewById(R.id.profile_pic_imageView)
         profileNameTextView = findViewById(R.id.profile_name_textView)
-        profileSurnameTextView = findViewById(R.id.profile_surname_textView)
+        profileEmailTextView = findViewById(R.id.profile_email_textView)
         profilePhonenoTextView = findViewById(R.id.profile_phoneno_textView)
         profileAddressTextView = findViewById(R.id.profile_address_textView)
         firebaseAuth = FirebaseAuth.getInstance()
         firebaseDatabase = FirebaseDatabase.getInstance()
         firebaseStorage = FirebaseStorage.getInstance()
-        btn_choose_image = findViewById<View>(R.id.btn_choose_image) as? Button
-        btn_upload_image = findViewById<View>(R.id.btn_upload_image) as? Button
+        btn_choose_image = findViewById<View>(R.id.btn_choose_image) as? ImageButton
         imagePreview = findViewById(R.id.profile_pic_imageView)
         firebaseStore = FirebaseStorage.getInstance()
         storageReference = FirebaseStorage.getInstance().reference
         btn_choose_image?.setOnClickListener { launchGallery() }
         btn_upload_image?.setOnClickListener { uploadImage() }
-        val databaseReference: DatabaseReference = firebaseDatabase!!.getReference(firebaseAuth!!.uid.toString())
+        val databaseReference: DatabaseReference =
+            firebaseDatabase!!.getReference(firebaseAuth!!.uid.toString())
         val storageReference: StorageReference = firebaseStorage!!.reference
         // Get the image stored on Firebase via "User id/Images/Profile Pic.jpg".
         firebaseAuth!!.uid?.let {
-            
+
             storageReference.child(it).child("Images").child("Profile Pic")
-                .downloadUrl.addOnSuccessListener(object : OnSuccessListener<Uri?> {
-                    
-                    override fun onSuccess(uri: Uri?) {
-                        
-                        // Using "Picasso" (http://square.github.io/picasso/) after adding the dependency in the Gradle.
-                        // ".fit().centerInside()" fits the entire image into the specified area.
-                        // Finally, add "READ" and "WRITE" external storage permissions in the Manifest.
-                        Picasso.get().load(uri).fit().centerInside().into(profilePicImageView)
-                    }
-                })
+                .downloadUrl.addOnSuccessListener { uri -> // Using "Picasso" (http://square.github.io/picasso/) after adding the dependency in the Gradle.
+                    // ".fit().centerInside()" fits the entire image into the specified area.
+                    // Finally, add "READ" and "WRITE" external storage permissions in the Manifest.
+                    Picasso.get().load(uri).fit().centerInside().into(profilePicImageView)
+                }
         }
-        
+
         if (firebaseAuth!!.currentUser == null) {
-            
+
             finish()
-            startActivity(Intent(applicationContext, SignInActivity::class.java))
+            startActivity(Intent(applicationContext, LoginActivity::class.java))
         }
-        
+
         val user: FirebaseUser? = firebaseAuth!!.currentUser
         databaseReference.addValueEventListener(object : ValueEventListener {
-            
+
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                
+
                 val userProfile: Userinformation? = dataSnapshot.getValue(Userinformation::class.java)
                 profileNameTextView?.text = userProfile?.userName
-                profileSurnameTextView?.text = userProfile?.userSurname
+                profileEmailTextView?.text = userProfile?.userEmail
                 profilePhonenoTextView?.text = userProfile?.userPhoneno
-                textViewemailname?.text = user?.email
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                
+
                 Toast.makeText(this@ProfileActivity, databaseError.code, Toast.LENGTH_SHORT)
                     .show()
             }
@@ -109,91 +102,117 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     fun buttonClickedEditName(view: View?) {
-        
+
         val inflater = layoutInflater
         val alertLayout: View = inflater.inflate(R.layout.layout_custom_dialog_edit_name, null)
         val etUsername = alertLayout.findViewById<EditText>(R.id.et_username)
         val alert: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this)
         alert.setTitle("Name Edit")
-        // this is set the view from XML inside AlertDialog.
+        // this is set the view from XML inside AlertDialog
         alert.setView(alertLayout)
         // disallow cancel of AlertDialog on click of back button and outside touch
         alert.setCancelable(false)
-        alert.setNegativeButton("Cancel",{ dialog, which -> })
-        alert.setPositiveButton("OK") { dialog, which ->
-            
+        alert.setNegativeButton("Cancel") { _, _ -> }
+        alert.setPositiveButton("OK") { _, _ ->
+
             val name = etUsername.text.toString()
-            val surname = profileSurnameTextView!!.text.toString()
+            val email = profileEmailTextView!!.text.toString()
             val phoneno = profilePhonenoTextView!!.text.toString()
             val address = profileAddressTextView!!.text.toString()
-            val userinformation = Userinformation(name, surname, phoneno)
+            val userinformation = Userinformation(name, email, phoneno, address)
             val user: FirebaseUser? = firebaseAuth?.currentUser
             databaseReference?.child(user!!.uid)?.setValue(userinformation)
             databaseReference?.child(user!!.uid)?.setValue(userinformation)
             etUsername.onEditorAction(EditorInfo.IME_ACTION_DONE)
         }
-        
+
         val dialog: android.app.AlertDialog? = alert.create()
         dialog?.show()
     }
 
     fun buttonClickedEditEmail(view: View?) {
-        
+
         val inflater = layoutInflater
-        val alertLayout: View = inflater.inflate(R.layout.layout_custom_dialog_edit_surname, null)
-        val etUserEmail = alertLayout.findViewById<EditText>(R.id.et_userSurname)
+        val alertLayout: View = inflater.inflate(R.layout.layout_custom_dialog_edit_email, null)
+        val etUserEmail = alertLayout.findViewById<EditText>(R.id.et_userEmail)
         val alert: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this)
         alert.setTitle("Email edit")
         alert.setView(alertLayout)
         alert.setCancelable(false)
-        
         alert.setNegativeButton("Cancel") { _, _ -> }
         alert.setPositiveButton("OK") { _, _ ->
-            
+
             val name = profileNameTextView!!.text.toString()
             val email = etUserEmail.text.toString()
             val phoneno = profilePhonenoTextView!!.text.toString()
             val address = profileAddressTextView!!.text.toString()
-            val userinformation = Userinformation(name, email, phoneno)
+            val userinformation = Userinformation(name, email, phoneno, address)
             val user: FirebaseUser? = firebaseAuth?.currentUser
             databaseReference?.child(user!!.uid)?.setValue(userinformation)
             databaseReference?.child(user!!.uid)?.setValue(userinformation)
             etUserEmail.onEditorAction(EditorInfo.IME_ACTION_DONE)
         }
-        
+
         val dialog: android.app.AlertDialog? = alert.create()
         dialog?.show()
     }
 
     fun buttonClickedEditPhoneNo(view: View?) {
-        
+
         val inflater = layoutInflater
-        val alertLayout: View = inflater.inflate(R.layout.layout_custom_dialog_edit_phoneno, null)
-        val etUserPhoneno = alertLayout.findViewById<EditText>(R.id.et_userPhoneno)
+        val alertLayout: View = inflater.inflate(R.layout.layout_custom_dialog_edit_contactinfo, null)
+        val etUserPhoneno = alertLayout.findViewById<EditText>(R.id.et_userContactno)
         val alert: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this)
         alert.setTitle("Contact info Edit")
         alert.setView(alertLayout)
         alert.setCancelable(false)
         alert.setNegativeButton("Cancel") { _, _ -> }
-        
         alert.setPositiveButton("OK") { _, _ ->
-            
+
             val name = profileNameTextView!!.text.toString()
-            val email = profileSurnameTextView!!.text.toString()
+            val email = profileEmailTextView!!.text.toString()
             val phoneno = etUserPhoneno.text.toString()
-            val userinformation = Userinformation(name, email, phoneno)
+            val address = profileAddressTextView!!.text.toString()
+            val userinformation = Userinformation(name, email, phoneno, address)
             val user: FirebaseUser? = firebaseAuth?.currentUser
             databaseReference?.child(user!!.uid)?.setValue(userinformation)
             databaseReference?.child(user!!.uid)?.setValue(userinformation)
             etUserPhoneno.onEditorAction(EditorInfo.IME_ACTION_DONE)
         }
-        
+
+        val dialog: android.app.AlertDialog? = alert.create()
+        dialog?.show()
+    }
+
+    fun buttonClickedEditAddress(view: View?) {
+
+        val inflater = layoutInflater
+        val alertLayout: View = inflater.inflate(R.layout.layout_custom_dialog_edit_address, null)
+        val etUserEmail = alertLayout.findViewById<EditText>(R.id.et_userAddress)
+        val alert: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this)
+        alert.setTitle("Address edit")
+        alert.setView(alertLayout)
+        alert.setCancelable(false)
+        alert.setNegativeButton("Cancel") { _, _ -> }
+        alert.setPositiveButton("OK") { _, _ ->
+
+            val name = profileNameTextView!!.text.toString()
+            val email = etUserEmail.text.toString()
+            val phoneno = profilePhonenoTextView!!.text.toString()
+            val address = profileAddressTextView!!.text.toString()
+            val userinformation = Userinformation(name, email, phoneno, address)
+            val user: FirebaseUser? = firebaseAuth?.currentUser
+            databaseReference?.child(user!!.uid)?.setValue(userinformation)
+            databaseReference?.child(user!!.uid)?.setValue(userinformation)
+            etUserEmail.onEditorAction(EditorInfo.IME_ACTION_DONE)
+        }
+
         val dialog: android.app.AlertDialog? = alert.create()
         dialog?.show()
     }
 
     private fun launchGallery() {
-        
+
         val intent = Intent()
         intent.type = "image/*"
         intent.action = Intent.ACTION_GET_CONTENT
@@ -201,45 +220,46 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        
+
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
-            
+
             if(data == null || data.data == null){
+
                 return
             }
 
             filePath = data.data
             try {
-                
+
                 val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
                 imagePreview.setImageBitmap(bitmap)
             }
-            
+
             catch (e: IOException) {
-                
+
                 e.printStackTrace()
             }
         }
     }
 
     private fun uploadImage(){
-        
+
         if(filePath != null){
-            
+
             val ref = storageReference?.child("myImages/" + UUID.randomUUID().toString())
             val uploadTask = ref?.putFile(filePath!!)
 
         }
-        
+
         else{
-            
+
             Toast.makeText(this, "Please Upload an Image", Toast.LENGTH_SHORT).show()
         }
     }
 
     fun navigateLogOut(v: View?) {
-        
+
         FirebaseAuth.getInstance().signOut()
         val inent = Intent(this, MainActivity::class.java)
         startActivity(inent)
